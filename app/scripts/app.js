1b99fff37880202eff60bab5ebf0191f2d5bfb96
Caleb <caleb.usiel.rn@gmail.com>
2014-09-12 17:36:04 -0500

    Agregando archivos yeoman y tema

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name htmakerOrApp
+ * @description
+ * # htmakerOrApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('htmakerOrApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
